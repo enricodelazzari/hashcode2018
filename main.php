da7e815e40682278a215245eba5dcd174155e6b9
enricodelazzari <enricodlz@gmail.com>
2018-03-01 18:41:44 +0000

    UPDATE add validation

--- main.php
+++ main.php
@@ -1,22 +1,23 @@
 <?php
 
-  // vars
-
+  // VARS
   $filename = $argv[1];
   $array = [];
 
   $array = fileToArray($filename);
   var_dump($array);
 
-  // functions
+  // FUNCTIONS
   function fileToArray($filename)
   {
-    $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    $array = [];
-    foreach ($lines as $line_num => $line) {
-      //echo "Line #{$line_num}: " . htmlspecialchars($line) . "\n";
-      array_push($array, explode(" ", $line));
+    if (file_exists($filename)) {
+      $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+      $array = [];
+      foreach ($lines as $line_num => $line) {
+        array_push($array, explode(" ", $line));
+        return $array;
+      }
+      return [];
     }
-    return $array;
   }
 ?>
